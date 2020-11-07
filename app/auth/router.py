7b92c927329e98778a47f14cfd8e189e36363c3d
Aquiles Lacruz <aquiles.lacruz@gmail.com>
2020-11-07 17:09:25 -0300

    Added the reload token route

--- app/auth/router.py
+++ app/auth/router.py
@@ -4,7 +4,8 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app import settings, get_db
-from . import config, authenticate_user, create_access_token, schemas
+from . import config, authenticate_user, create_access_token, schemas, get_current_user
+from app.users import schemas as user_schemas
 
 
 router = APIRouter()
@@ -14,6 +15,19 @@ LOGIN_EXCEPTION = HTTPException(
     detail='Incorrect username or password',
     headers={"WWW-Authenticate": "Bearer"}
 )
+
+
+@router.get('/', response_model=schemas.Token)
+async def reload(user: user_schemas.User = Depends(get_current_user)):
+    access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={'sub': user.username},
+        expires_delta=access_token_expires
+    )
+    return {
+        'access_token': access_token,
+        'token_type': 'bearer'
+    }
 
 
 @router.post('/', response_model=schemas.Token)
